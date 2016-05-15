f64055efa6a407c107cded22200a67ec17071b15
AshMorrow <koragg666@gmail.com>
2016-05-15 23:56:40 +0300

    update parser

---
+++ libs/ParseStart.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: KoRaG
+ * Date: 15.05.2016
+ * Time: 23:06
+ */
+
+namespace libs;
+use libs\PokedexData;
+use libs\PokemonGetData;
+use model\PokedexModel;
+
+class ParseStart
+{
+    
+}
